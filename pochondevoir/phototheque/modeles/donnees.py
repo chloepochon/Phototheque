7538e14f966608c118a323937e05887953adc4b4
chloepochon <74970056+chloepochon@users.noreply.github.com>
2021-03-08 20:44:36 +0100

    Imports modeles

---
+++ pochondevoir/phototheque/modeles/donnees.py
@@ -0,0 +1,26 @@
+import datetime
+from .. app import db
+
+# On configure le modèle de la table Photo
+class Photo(db.Model):
+    photo_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    photo_titre = db.Column(db.Text)
+    photo_description = db.Column(db.Text)
+    photo_auteur = db.Column(db.Text)
+    photo_date = db.Column(db.Text)
+    photo_tag = db.Column(db.Text)
+    photo_orientation = db.Column(db.String(8))
+    photo_chemin = db.Column(db.String(64), index=True, unique=True)
+    photo_fichier = db.Column(db.String(64), index=True, unique=True)
+    authorships = db.relationship("Authorship", back_populates="photo")
+
+# On configure le modèle de la table Authorship
+class Authorship(db.Model):
+    __tablename__ = "authorship"
+    authorship_id = db.Column(db.Integer, nullable=True, autoincrement=True, primary_key=True)
+    authorship_photo_id = db.Column(db.Integer, db.ForeignKey('photo.photo_id'))
+    authorship_user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    authorship_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    user = db.relationship("User", back_populates="authorships")
+    photo = db.relationship("Photo", back_populates="authorships")
+
